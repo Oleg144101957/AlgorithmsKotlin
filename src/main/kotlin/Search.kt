b73b1457bd76157002b7663e4eb1c014b94661be
Oleg <vishnevskiyoleg@gmail.com>
2022-06-20 15:32:02 +0300

    Recursion added

--- src/main/kotlin/Search.kt
+++ src/main/kotlin/Search.kt
@@ -2,13 +2,11 @@
 var counter = 0
 
 fun main(args: Array<String>){
-
     val input = arrayOf(1,2,3,4,5,6,7,8,9)
-    val lookingFor = 8
-
-    println(binarSerchWhile(input, lookingFor))
+    val lookingFor = 2
 
 
+    binarSearchRecursion(input, lookingFor, 0, high = input.size-1)
 
 }
 
@@ -23,7 +21,7 @@ fun lineSearch(input: Array<Int>, lookingFor: Int){
 
 }
 
-fun binarSerchWhile(input: Array<Int>, lookingFor: Int) : String {
+fun binarSearchWhile(input: Array<Int>, lookingFor: Int) : String {
 
     var low = 0
     var high = input.size-1
@@ -40,5 +38,50 @@ fun binarSerchWhile(input: Array<Int>, lookingFor: Int) : String {
         }
     }
     return "Not found"
+
+}
+
+
+fun binarSearchWhile2(input: Array<Int>, lookingFor: Int){
+    var low = 0
+    var high = input.size-1
+
+    while (low<=high){
+        val middle = low+(high-low)/2
+
+        if (input[middle] == lookingFor){
+            println("Found index $middle")
+            return
+        }
+
+        if (input[middle]>lookingFor){
+            high = middle-1
+        } else {
+            low = middle +1
+        }
+    }
+    println("Not found")
+
+}
+
+fun binarSearchRecursion(input: Array<Int>, lookingFor: Int, low: Int, high: Int){
+
+    val mid = low+(high-low)/2
+
+    if (input[mid] == lookingFor){
+        println("Index found $mid")
+        return
+    }
+
+    if (low>=high){
+        println("Not found")
+        return
+    }
+
+    if (input[mid]>lookingFor){
+        binarSearchRecursion(input, lookingFor, low, mid-1)
+    } else {
+        binarSearchRecursion(input, lookingFor, mid+1, high)
+    }
 
 }
